1965c2455a3497d268d393b667c3399b6af2db1e
Tanya <132035563+tanya1829@users.noreply.github.com>
2025-06-23 09:59:02 +0530

    Add files via upload

---
+++ Java_programmingLevel_1/AverageMarks.java
@@ -0,0 +1,9 @@
+class AverageMarks {
+    public static void main(String[] args) {
+        int maths = 94, physics = 95, chemistry = 96;
+        int totalMarks = maths + physics + chemistry;
+        double average = totalMarks / 3.0;
+
+        System.out.println("Sam’s average mark in PCM is " + average);
+    }
+}
