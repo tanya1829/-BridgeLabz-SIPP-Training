a6ca43d018c006c0045b9cdbaec2bcde3ecc2418
Tanya <132035563+tanya1829@users.noreply.github.com>
2025-07-29 22:21:44 +0530

    Add files via upload

---
+++ Collections/RotateList.java
@@ -0,0 +1,21 @@
+package JavaGenerics.Collections;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class RotateList {
+    public static <T> void rotateList(List<T> list, int k) {
+        int n = list.size();
+        k = k % n;
+        List<T> rotated = new ArrayList<>(list.subList(k, n));
+        rotated.addAll(list.subList(0, k));
+        for (int i = 0; i < n; i++) list.set(i, rotated.get(i));
+    }
+
+    public static void main(String[] args) {
+        ArrayList<Integer> rotateList = new ArrayList<>(Arrays.asList(10, 20, 30, 40, 50));
+        rotateList(rotateList, 2);
+        System.out.println("Rotated List by 2: " + rotateList);
+    }
+}
